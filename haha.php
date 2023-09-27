39da3dd2aa521891d464e7966648a7c7ced5ca2d
NIJITSO <NIJITSO@users.noreply.github.com>
2023-09-27 12:47:36 +0100

    d

--- haha.php
+++ haha.php
@@ -1,135 +1,104 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
+    <title>Product Selection</title>
 </head>
 <body>
+    <?php
+    // Handle form submission
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $selectedProduct = $_POST["product"];
+        $selectedSize = $_POST["size"];
 
-<h2>Modal Example</h2>
-<?php
-require('set.php');
-$sql = "SELECT * FROM product";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-while ($row = mysqli_fetch_assoc($result)) {
-?>
-<!-- Trigger/Open The Modal -->
-<button class="myBtn" data-id="<?=$row['idP']?>">Open Modal</button>
-<?php }} ?>
+        // You can now process the selected product and size as needed, e.g., insert them into another table or perform some action.
+        // Make sure to handle validation and database interactions securely.
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
-  <!-- Modal content -->
-  <div class="modal-content">
+        // Redirect or display a confirmation message as needed.
+        echo "Selected Product ID: " . $selectedProduct . "<br>";
+        if (!empty($selectedSize)) {
+            echo "Selected Size ID: " . $selectedSize;
+        } else {
+            echo "No Size Selected";
+        }
+    }
+    ?>
 
-    <span class="close">&times;</span>
-    <p>You clicked <span id="clickedId"></span></p>
+    <form method="post" action="">
+        <label for="product">Select a Product:</label>
+        <select name="product" id="product">
+            <?php
+            require('set.php');
 
-    <table>
-      <tr>
-        <th>idP</th>
-        <th>nameP</th>
-        <th>sizeValue</th>
-        <th>qty</th>
-      </tr>
-      <?php
-      $q="SELECT p.idP AS `idX` ,p.nameP AS `nameX` , s.sizeValue AS `sizeValueX`, pd.qty AS `qtyX`
-          FROM product_detail pd
-          JOIN product p ON pd.idP = p.idP
-          JOIN size s ON pd.idSize = s.idSize
-          WHERE p.idP=2";
-          $result2 = mysqli_query($conn, $q);
-          if (mysqli_num_rows($result2) > 0) {
-          while ($row2 = mysqli_fetch_assoc($result2)) {
-      ?>
-      <tr>
-        <th><?=$row2['idX']?></th>
-        <th><?=$row2['nameX']?></th>
-        <th><?=$row2['sizeValueX']?></th>
-        <th><?=$row2['qtyX']?></th>
-      </tr>
-      <?php }} ?>
-    </table>
-  </div>
-</div>
+            // Retrieve products from the database
+            $sql = "SELECT idP, nameP, hasVariables FROM product";
+            $result = $conn->query($sql);
 
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $productId = $row["idP"];
+                    $productName = $row["nameP"];
+                    $hasVariables = $row["hasVariables"];
+                    
+                    echo "<option value='$productId' data-hasvariables='$hasVariables'>$productName</option>";
+                }
+            }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+            $conn->close();
+            ?>
+        </select>
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+        <!-- Create the sizes input if the selected product has variables -->
+        <div id="sizesDiv" style="display: none;">
+            <label for="size">Select Size:</label>
+            <select name="size" id="size">
+                <?php
+                // Assuming you have a database connection established
+                $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Function to open the modal with the clicked ID
-function openModal(id) {
-  var modalContent = document.getElementById("clickedId");
-  modalContent.textContent = id;
-  modal.style.display = "block";
-}
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
 
-// Add click event listeners to all buttons with class "myBtn"
-var buttons = document.querySelectorAll(".myBtn");
-buttons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    var id = this.getAttribute("data-id");
-    openModal(id);
-  });
-});
+                // Retrieve sizes from the database
+                $sql = "SELECT idSize, sizeValue FROM size";
+                $result = $conn->query($sql);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $sizeId = $row["idSize"];
+                        $sizeValue = $row["sizeValue"];
+                        
+                        echo "<option value='$sizeId'>$sizeValue</option>";
+                    }
+                }
 
+                $conn->close();
+                ?>
+            </select>
+        </div>
+
+        <input type="submit" value="Submit">
+    </form>
+
+    <script>
+        // JavaScript to show/hide the sizes input based on product selection
+        document.getElementById("product").addEventListener("change", function () {
+            var productSelect = document.getElementById("product");
+            var sizesDiv = document.getElementById("sizesDiv");
+
+            if (productSelect.value !== "") {
+                var selectedOption = productSelect.options[productSelect.selectedIndex];
+                var hasVariables = selectedOption.getAttribute("data-hasvariables");
+
+                if (hasVariables === "1") {
+                    sizesDiv.style.display = "block";
+                } else {
+                    sizesDiv.style.display = "none";
+                }
+            } else {
+                sizesDiv.style.display = "none";
+            }
+        });
+    </script>
 </body>
 </html>
